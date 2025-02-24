8e9d322a8c7648a697149b59e33fa179990cc2aa
btdmaster1563 <btdmaster1563@gmail.com>
2025-02-23 19:31:41 -0800

    progresss before switch branch

--- indexer.py
+++ indexer.py
@@ -1,4 +1,17 @@
+import json
 import os
+from nltk.stem import PorterStemmer
+from bs4 import BeautifulSoup
+
+stemmer = PorterStemmer()
+
 for dirpath, dirnames, filenames in os.walk("developer"):
     for filename in filenames:
         actual_rel_name = os.path.join(dirpath, filename)
+        with open(actual_rel_name,"r") as file:
+            jsonObj = json.load(file) 
+            soup = BeautifulSoup(jsonObj.get("content"), features="html.parser")
+            visible_text = soup.getText(" ").split()
+            for text in visible_text:
+                text = text.strip()
+                
--- merger.py
+++ merger.py
@@ -1,33 +1,50 @@
 # open all three partial index files so we can iterate through them
-with open("partial_index_1", "r", 1) as file1, open("partial_index_1", "r", 1) as file2, open("partial_index_1", "r", 1) as file3:
-    # creates an iterator for each file that we will use to loop through each file
+with open("partial_index_1", "r", 1, encoding="utf-8") as file1, open("partial_index_2", "r", 1, encoding="utf-8") as file2, open("partial_index_3", "r", 1, encoding="utf-8") as file3:
+    
+    # create an iterator for each file that we will use to loop through each file
     part1 = iter(file1)
     part2 = iter(file2)
     part3 = iter(file3)
+
+    #create three boolean switches to keep track of changes to each iterator
     switch1 = True
     switch2 = True
     switch3 = True
+
+    #init 3 strings that will store a line from each file
     line1 = ""
     line2 = ""
     line3 = ""
+
+    #loop until EOF on all files
     while True:
         try:
             if switch1:
                 line1 = next(part1).strip()
         except StopIteration:
             line1 = ""
+            switch1 = False
 
         try:
             if switch2:
                 line2 = next(part2).strip()
         except StopIteration:
-            line1 = ""
+            line2 = ""
+            switch2 = False
 
         try:
             if switch3:
                 line3 = next(part3).strip()
         except StopIteration:
-            line1 = ""
+            line3 = ""
+            switch3 = False
         
-        if not line1 and not line2 and not line3:
+        if not (line1 or line2 or line3):
+            print("finished?")
             break
+        
+        word1 = line1.split[0]
+        word2 = line2.split[0]
+        word3 = line3.split[0]
+
+        if word1 == word2
