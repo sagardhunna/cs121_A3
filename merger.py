2a6becc4e753e44436d3122ac1b1bdb33e7ebc9d
btdmaster1563 <btdmaster1563@gmail.com>
2025-02-24 00:21:19 -0800

    fix merger

--- merger.py
+++ merger.py
@@ -38,55 +38,56 @@ with open("partial_indexes/partial_index_1.txt", "r", encoding="utf-8") as file1
         if line3 != "":
             word3 = line3.split()[0]
 
-        if word1 < word2 and word1 < word3:
-            writer.write(line1)
-            try:
-                line1 = next(part1).strip()
-            except StopIteration:
-                line1 = ""
-                switch1 = True
-            continue
-
-        if word2 < word1 and word2 < word3:
-            writer.write(line2)
-            try:
-                line2 = next(part2).strip()
-            except StopIteration:
-                line2 = ""
-                switch2 = True
-            continue
-
-        if word3 < word1 and word3 < word2:
-            writer.write(line3)
-            try:
-                line3 = next(part3).strip()
-            except StopIteration:
-                line3 = ""
-                switch3 = True
-            continue
-        
-        if word1==word2:
-            if word1==word3: #1==2==3
-                writer.write(line1+line2[len(word2):]+line3[len(word3):])
+        if word1!="":
+            if word1 < word2 and word1 < word3:
+                writer.write(line1)
                 try:
                     line1 = next(part1).strip()
                 except StopIteration:
                     line1 = ""
                     switch1 = True
-
+                continue
+        if word2!="":
+            if word2 < word1 and word2 < word3:
+                writer.write(line2)
                 try:
                     line2 = next(part2).strip()
                 except StopIteration:
                     line2 = ""
                     switch2 = True
-
+                continue
+        if word2!="":
+            if word3 < word1 and word3 < word2:
+                writer.write(line3)
                 try:
                     line3 = next(part3).strip()
                 except StopIteration:
                     line3 = ""
                     switch3 = True
                 continue
+        
+        if word1 != "":
+            if word1==word2:
+                if word1==word3: #1==2==3
+                    writer.write(line1+line2[len(word2):]+line3[len(word3):])
+                    try:
+                        line1 = next(part1).strip()
+                    except StopIteration:
+                        line1 = ""
+                        switch1 = True
 
+                    try:
+                        line2 = next(part2).strip()
+                    except StopIteration:
+                        line2 = ""
+                        switch2 = True
+
+                    try:
+                        line3 = next(part3).strip()
+                    except StopIteration:
+                        line3 = ""
+                        switch3 = True
+                    continue
             else: #1==2
                 writer.write(line1+line2[len(word2):])
                 try:
@@ -102,34 +103,34 @@ with open("partial_indexes/partial_index_1.txt", "r", encoding="utf-8") as file1
                     switch2 = True
                 continue
         
-        if word1==word3: #1==3
-            writer.write(line1+line3[len(word3):])
-            try:
-                line1 = next(part1).strip()
-            except StopIteration:
-                line1 = ""
-                switch1 = True
-            
-            try:
-                line3 = next(part3).strip()
-            except StopIteration:
-                line3 = ""
-                switch3 = True
-            continue
+            if word1==word3: #1==3
+                writer.write(line1+line3[len(word3):])
+                try:
+                    line1 = next(part1).strip()
+                except StopIteration:
+                    line1 = ""
+                    switch1 = True
+                
+                try:
+                    line3 = next(part3).strip()
+                except StopIteration:
+                    line3 = ""
+                    switch3 = True
+                continue
+        if word1 != "":
+            if word2==word3: #2==3
+                writer.write(line2+line3[len(word3):])
+                try:
+                    line2 = next(part2).strip()
+                except StopIteration:
+                    line2 = ""
+                    switch2 = True
 
-        if word2==word3: #2==3
-            writer.write(line2+line3[len(word3):])
-            try:
-                line2 = next(part2).strip()
-            except StopIteration:
-                line2 = ""
-                switch2 = True
-
-            try:
-                line3 = next(part3).strip()
-            except StopIteration:
-                line3 = ""
-                switch3 = True
-            continue
+                try:
+                    line3 = next(part3).strip()
+                except StopIteration:
+                    line3 = ""
+                    switch3 = True
+                continue
     writer.close()
     print(count)
