9896bf7fa7f073840a1cac190eb155d99816c96a
Sagar Dhunna <sagardhunna17@gmail.com>
2025-02-23 20:13:12 -0800

    just some comments

--- indexer.py
+++ indexer.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 
 stemmer = PorterStemmer()
 
+word_to_doc_freq = {}
+# {word: [(doc#, freq)]}
+# map with key = token and value being a set of tuples (doc, frequency)
 for dirpath, dirnames, filenames in os.walk("developer"):
     for filename in filenames:
         actual_rel_name = os.path.join(dirpath, filename)
@@ -12,6 +15,7 @@ for dirpath, dirnames, filenames in os.walk("developer"):
             jsonObj = json.load(file) 
             soup = BeautifulSoup(jsonObj.get("content"), features="html.parser")
             visible_text = soup.getText(" ").split()
+            # visible_text is a list of all tokens in file
             for text in visible_text:
                 text = text.strip()
                 
