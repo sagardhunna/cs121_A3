ab41df9d309d9c12288be289811c32a258a52c03
btdmaster1563 <btdmaster1563@gmail.com>
2025-03-06 01:28:52 -0800

    reworked merger to be like 100x faster

--- merger.py
+++ merger.py
@@ -1,54 +1,88 @@
 import os
-import re
-
-folder_path = "rough_indexes"
-
-output_folder = "partial_indexes"
-
-os.makedirs(output_folder, exist_ok=True) # making sure it exists
-
-letter_map = {}
-count = 0
+import operator
+import time
 
 def merge_duplicates():
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".txt"):
-            file_path = os.path.join(folder_path, filename)
-            with open(file_path, "r") as file:
-                for line in file:
-                    line = line.lstrip()
-                    match = re.search(r"^\S+", line)  # searches all words in a space
-                    word = match.group(0) if match else None  # finds the first one
+    files = list()
+    streams = list()
+    iterWords = list()
+    write_file = open("partial_indexes/partial_index_0.txt","wt")
+    filenum = 1
+    while 1:
+        if os.path.exists(f"rough_indexes/rough_ind_{filenum}.txt"):
+            file_path = os.path.join(f"rough_indexes/rough_ind_{filenum}.txt")
+            files.append(file_path)
+        else:
+            break
+        filenum+=1
+    for f in files:
+        streams.append(open(f, "r", 1, encoding="utf-8"))
+    for s in streams:
+        temp = list()
+        it = iter(s)
+        temp.append(next(it))
+        temp.append(it)
+        iterWords.append(temp)
 
-                    if word is None:
-                        continue
+    iterWords.sort(key=operator.itemgetter(0))
+    
+    count = 0
+    prev_ltr = "0"
+    while iterWords:
+        count+=1
 
-                    docs = re.findall(r"(doc\d+,\d+)", line)  # checks the doc1,1
+        if len(iterWords)==1:
+            write_file.write(iterWords[0][0].strip())
+            try:
+                iterWords[0][0] = next(iterWords[0][1])
+            except StopIteration:
+                iterWords.clear()
+                break
+            continue
 
-                    if word not in letter_map.keys():
-                        letter_map[word] = set(docs)  # starts the map
-                    else:
-                        letter_map[word].update(docs)  # Append list of docs
+        iterWords.sort(key=operator.itemgetter(0))
 
-        sorted_letter_map = {
-            word: sorted(docs, key=lambda doc: int(re.search(r"doc(\d+)", doc).group(1)))
-            for word, docs in sorted(letter_map.items())
-        }
 
-        for word, docs in sorted_letter_map.items():
-            partial_file = os.path.join(output_folder, f"partial_index_{word[0]}.txt")
+        currLine = iterWords[0][0].strip()
+        word = currLine[:currLine.find(" ")]
 
-            with open(partial_file, "a") as file:
-                file.write(f'{word}')
-                for doc in docs:  # `docs` is a list of strings
-                    file.write(f' {doc}')
-                file.write('\n')
+        if prev_ltr != word[0]: 
+            write_file.close()
+            write_file = open(f"partial_indexes/partial_index_{word[0]}.txt","wt")
+            prev_ltr = word[0]
 
-    letter_map.clear()
+        word = currLine[:currLine.find(" ")]
+        to_remove = list()
+        for i in range(1,len(iterWords)):
+            linei = iterWords[i][0].strip()
+            if word != linei[:linei.find(" ")]:
+                break
+            currLine = currLine + linei[len(word):]
+            try:
+                iterWords[i][0] = next(iterWords[i][1])
+            except StopIteration:
+                to_remove.append(i)
+        try:
+            iterWords[0][0] = next(iterWords[0][1])
+        except StopIteration:
+            to_remove.insert(0,0)
+        for i in reversed(to_remove):
+            iterWords.pop(i)
+        write_file.write(currLine+"\n")
+            
 
+    for s in streams:
+        s.close()
+    write_file.close()
+    return count
 
 def main():
-    merge_duplicates()
+    start = time.perf_counter()
+    for i in range(0,1):
+        count = merge_duplicates()
+    end = time.perf_counter()
+    print(f'runtime: {end-start} seconds')
+    print(f'unique tokens: {count}')
 
 
 if __name__ == "__main__":
